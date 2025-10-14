a608be7dba429e8b86bf77eb2b4b0eda0b95cb53
Oumer Jemal <oumerjemal293@gmail.com>
2025-10-14 11:55:45 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Projects from './pages/Projects'
+import Header from './components/Header'
+
+
+function App() {
+ 
+
+  return (
+    
+     <div>
+      <Header />
+      <Routes>
+        <Route path='/' element = {<Home />} />
+        <Route path='/about' element = {<About />} />
+        <Route path='/projects' element = {<Projects />} />
+        <Route path='/contact' element = {<Contact/>} />        
+      </Routes>
+     </div>
+    
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = ()=>{
+    return (
+        <div>
+            <img src="./Mylogo.png" alt="mylogo" />
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+            <button>Learn more</button>
+        </div>
+    )
+}
+
+export default Header
